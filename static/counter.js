bfdb4fc6f08d12bc68cdd595e0d97f0d7ebffb77
Grok <devanaptaker@gmail.com>
2026-05-12 14:10:59 -0700

    add: event listeners

--- static/counter.js
+++ static/counter.js
@@ -1,4 +1,12 @@
+document.getElementById("counter-up").addEventListener("click", counterUp);
+document.getElementById("counter-down").addEventListener("click", counterDown);
+document.getElementById("counter-reset").addEventListener("click", counterReset);
+document.getElementById("counter-save").addEventListener("click", counterSave);
+document.getElementById("counter-load").addEventListener("click", counterLoad);
+
 let count = 0;
+
+counterLoad();
 
 function updateCounter(){
     document.getElementById("counter").textContent = count
@@ -19,11 +27,11 @@ function counterReset(){
     count = 0;
     updateCounter()
 }
-function saveCount(){
+function counterSave(){
     localStorage.setItem("count", count);
     console.log(count)
 }
-function loadCount(){
+function counterLoad(){
     count = localStorage.getItem("count");
     console.log(count)
     updateCounter()
