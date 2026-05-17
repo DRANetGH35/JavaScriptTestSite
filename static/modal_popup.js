cc11e2f17e75eca141260d523046950232802096
Grok <devanaptaker@gmail.com>
2026-05-16 18:54:26 -0700

    add: clickaway and escape to close popup

--- static/modal_popup.js
+++ static/modal_popup.js
@@ -6,6 +6,18 @@ const closeBtn = document.getElementById('closeBtn')
 openBtn.addEventListener('click', openModalPopup)
 closeBtn.addEventListener('click', closeModalPopup)
 
+modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+        closeModalPopup();
+    }
+})
+
+document.addEventListener("keydown", function (event){
+    if (event.key === "Escape") {
+        closeModalPopup();
+    }
+})
+
 function openModalPopup(){
     console.log('opening')
     modal.classList.add('show')
