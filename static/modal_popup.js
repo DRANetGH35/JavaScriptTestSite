a25650985183d2b907280f43f88393b336407194
Grok <devanaptaker@gmail.com>
2026-05-18 11:17:14 -0700

    add: trap focus

--- static/modal_popup.js
+++ static/modal_popup.js
@@ -1,10 +1,30 @@
 
 const modal = document.getElementById('modal')
+const modal_box = document.getElementById("modal-box")
 const openBtn = document.getElementById('openBtn')
 const closeBtn = document.getElementById('closeBtn')
 
+const focusableEls = modal.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+const firstFocusable = focusableEls[0];
+const lastFocusable = focusableEls[focusableEls.length - 1];
+
 openBtn.addEventListener('click', openModalPopup)
 closeBtn.addEventListener('click', closeModalPopup)
+
+modal.addEventListener('keydown', (e) => {
+    if (e.key != "Tab") return;
+    if (e.shiftKey) {
+        if (document.activeElement === firstFocusable) {
+            lastFocusable.focus();
+            e.preventDefault();
+        }
+    } else {
+        if (document.activeElement === lastFocusable) {
+            firstFocusable.focus();
+            e.preventDefault();
+        }
+    }
+});
 
 modal.addEventListener("click", function (event) {
     if (event.target === modal) {
@@ -19,8 +39,9 @@ document.addEventListener("keydown", function (event){
 })
 
 function openModalPopup(){
-    console.log('opening')
-    modal.classList.add('show')
+    modal.classList.add('show');
+    modal_box.focus();
+    console.log(document.activeElement);
 }
 
 function closeModalPopup(){
